50eb67055052cff2b6bbfd94fb44d963bf1fa03b
migoy99 <113483557+migoy99@users.noreply.github.com>
2023-11-01 16:09:06 +0800

    added menu option added margin when viewing single journal

--- resources/views/view-journal.blade.php
+++ resources/views/view-journal.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="mt-4 mx-20 content-center">
+    <div class="mx-20 content-center mt-20 mb-20">
         <h5 class="text-3xl font-extrabold dark:text-white">{!! $journal->title !!}</h5>
         <p class="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">{!! $journal->author !!},
             {{ date('M Y', strtotime($journal->created_at)) }}</p>
