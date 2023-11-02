50567d581ab4c33e63030ff1e0630708ac36c8b2
migoy99 <113483557+migoy99@users.noreply.github.com>
2023-11-02 22:10:41 +0800

    format tailwind classes

--- resources/views/create-journal.blade.php
+++ resources/views/create-journal.blade.php
@@ -30,8 +30,8 @@
 
             </div>
             <button type="submit"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 
-                focus:outline-none focus:ring-blue-300 font-medium rounded-lg 
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4
+                focus:outline-none focus:ring-blue-300 font-medium rounded-lg
                 text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700
                 dark:focus:ring-blue-800">
                 Publish journal
