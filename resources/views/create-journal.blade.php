737a7a14eb82964666f7bd0baaa18a3fe79b2ce1
migoy99 <113483557+migoy99@users.noreply.github.com>
2023-11-02 22:08:24 +0800

    format tailwind classes

--- resources/views/create-journal.blade.php
+++ resources/views/create-journal.blade.php
@@ -9,7 +9,11 @@
                     Your journal title
                 </label>
                 <input type="text" id="title" name="title"
-                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900
+                    text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block
+                    w-full p-2.5 dark:bg-gray-700 dark:border-gray-600
+                    dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
+                    dark:focus:border-blue-500 dark:shadow-sm-light"
                     placeholder="" required>
             </div>
             <div class="mb-6">
@@ -18,12 +22,18 @@
                     Your journal content
                 </label>
                 <textarea name="contents" rows="15" id="contents" rows="4"
-                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg
+                     border border-gray-300 focus:ring-blue-500 focus:border-blue-500
+                     dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
+                     dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="this supports markdown..." required></textarea>
 
             </div>
             <button type="submit"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 
+                focus:outline-none focus:ring-blue-300 font-medium rounded-lg 
+                text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700
+                dark:focus:ring-blue-800">
                 Publish journal
             </button>
         </form>
