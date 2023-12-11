f088322c29e1481443cf1876e7c63a5c9af6e32a
migoy99 <113483557+migoy99@users.noreply.github.com>
2023-12-11 11:05:57 +0800

    removed form div bottom margin added publish button top margin

--- resources/views/create-journal.blade.php
+++ resources/views/create-journal.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <form method="POST" action="{{ route('savejournal') }}" id="publishJournalForm">
-        <div class="mt-20 mb-20">
+        <div class="mt-20">
             @csrf
             <div class="mb-2">
                 <label for="title" class="block mb-2 text-sm font-medium text-gray-900">
@@ -24,7 +24,7 @@
         <button type="submit"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4
             focus:outline-none focus:ring-blue-300 font-medium rounded-lg
-            text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700
+            text-sm px-5 py-2.5 mt-5 text-center dark:bg-blue-600 dark:hover:bg-blue-700
           dark:focus:ring-blue-800">
             Publish
         </button>
