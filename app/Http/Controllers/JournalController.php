ba4bf674fe5cc218ed66dfae4eadb270141ce62d
migoy99 <113483557+migoy99@users.noreply.github.com>
2023-12-21 08:08:22 +0800

    applied table of contents to articles

--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -18,7 +18,7 @@ class JournalController extends Controller
      */
     public static function returnCreateJournalView()
     {
-         return view('create-journal');
+        return view('create-journal');
     }
     public static function getAllJournal()
     {
@@ -46,19 +46,46 @@ class JournalController extends Controller
     {
         $journal = Journal::getSingleJournal($id);
         $parsedJournal = Journal::journalContentParseToMarkdown($journal);
+
         return view('view-journal', [
             "parsedContents" => $parsedJournal,
             "journal" => $journal,
-            "journals" => Journal::getAllJournal()
+            "journals" => Journal::getAllJournal(),
         ]);
     }
     public static function parseSingleJournal($id)
     {
         $journal = Journal::getSingleJournal($id);
         $markdownContent = Journal::journalContentParseToMarkdown($journal);
-    
+
         // Limit
         $lines = explode("\n", $markdownContent);
         return implode("\n", array_slice($lines, 0, 1));
+    }
+
+    // FOR TABLE OF CONTENTS
+    public static function generateIndex($journalContents)
+    {
+
+        preg_match_all('/^#{1,3}\s+(.*?)$/m', $journalContents, $matches);
+
+        $index = "<ul class='max-w-md space-y-1 text-gray-500 list-none list-inside dark:text-gray-400'>";
+
+        foreach ($matches[0] as $i => $match) {
+            $text = trim($matches[1][$i]);
+            $slug = strtolower(str_replace("--", "-", preg_replace('/[^\da-z]/i', '-', $text)));
+            $id = 'section-' . $slug;
+
+            // Add id attribute to the corresponding section
+            $anchor = '<a id="' . $id . '">' . $text . '</a>';
+            $journalContents = str_replace($match, $anchor, $journalContents);
+
+            $index .= '<li><a href="#' . $id . '">' . $text . '</a></li>';
+        }
+
+        $index .= "</ul>";
+
+        return ["html" => $journalContents, "index" => $index];
+
     }
 }
